0e2c7b2bd2599f0044d9fa3783ca98cc525e254c
Darwin D Wu <darwin67@users.noreply.github.com>
2025-08-02 23:46:19 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Email client";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            flutter
+
+            # tools
+            git-cliff
+            claude-code
+            gemini-cli
+          ];
+        };
+      });
+}
