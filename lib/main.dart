7c822e1a9ba179186cb33eb29ff35ff83cc856e2
Darwin D Wu <darwin67@users.noreply.github.com>
2025-08-05 22:55:55 -0700

    update deps

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart' hide ThemeData;
 import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/auth_provider.dart';
+import 'screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,110 +12,89 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ShadApp(
-      title: 'Flutter Demo',
-      theme: ShadThemeData(
-        brightness: Brightness.light,
-        colorScheme: const ShadSlateColorScheme.light(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: ShadApp(
+        title: 'Meeru - Email Client',
+        theme: ShadThemeData(
+          brightness: Brightness.light,
+          colorScheme: const ShadSlateColorScheme.light(),
+        ),
+        darkTheme: ShadThemeData(
+          brightness: Brightness.dark,
+          colorScheme: const ShadSlateColorScheme.dark(),
+        ),
+        home: const AppRoot(),
       ),
-      darkTheme: ShadThemeData(
-        brightness: Brightness.dark,
-        colorScheme: const ShadSlateColorScheme.dark(),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class AppRoot extends StatefulWidget {
+  const AppRoot({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AppRoot> createState() => _AppRootState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class _AppRootState extends State<AppRoot> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<AuthProvider>(context, listen: false).loadAccounts();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ShadTheme.of(context).colorScheme.background,
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          children: [
-            ShadCard(
-              padding: const EdgeInsets.all(24),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Consumer<AuthProvider>(
+      builder: (context, authProvider, child) {
+        if (authProvider.isLoading) {
+          return Scaffold(
+            backgroundColor: ShadTheme.of(context).colorScheme.background,
+            body: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        if (authProvider.hasAccounts) {
+          // TODO: Navigate to main email interface
+          return Scaffold(
+            backgroundColor: ShadTheme.of(context).colorScheme.background,
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  const Icon(Icons.check_circle, size: 64, color: Colors.green),
+                  const SizedBox(height: 16),
                   Text(
-                    widget.title,
-                    style: ShadTheme.of(context).textTheme.h3,
+                    'Account Setup Complete!',
+                    style: ShadTheme.of(context).textTheme.h2,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'You have ${authProvider.accounts.length} account(s) configured.',
+                    style: ShadTheme.of(context).textTheme.p,
+                  ),
+                  const SizedBox(height: 24),
+                  ShadButton(
+                    onPressed: () => authProvider.loadAccounts(),
+                    child: const Text('Refresh'),
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 24),
-            Expanded(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'You have pushed the button this many times:',
-                      style: ShadTheme.of(context).textTheme.p,
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      '$_counter',
-                      style: ShadTheme.of(context).textTheme.h1,
-                    ),
-                    const SizedBox(height: 32),
-                    ShadButton(
-                      onPressed: _incrementCounter,
-                      child: const Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Icons.add),
-                          SizedBox(width: 8),
-                          Text('Increment'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          );
+        }
+
+        return const WelcomeScreen();
+      },
     );
   }
 }
