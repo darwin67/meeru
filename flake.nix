d9c4f09e11780e4b2bc6238ec9787c5eb3bf68ad
Darwin D Wu <darwin67@users.noreply.github.com>
2025-11-14 22:53:20 -0800

    add flake for deps

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Meeru - Cross Platform Email App";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        corepack = pkgs.stdenv.mkDerivation {
+          name = "corepack";
+          buildInputs = [ pkgs.nodejs_24 ];
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out/bin
+            corepack enable --install-directory=$out/bin
+          '';
+        };
+
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [ corepack ];
+
+          buildInputs = with pkgs; [
+            rustc
+            rustup
+            rustfmt
+            cargo
+            clippy
+
+            # Lua
+            lua
+
+            # Node
+            typescript
+            nodejs_24
+
+            # LSPs
+            rust-analyzer
+            nodePackages.typescript-language-server
+            nodePackages.vscode-json-languageserver
+            nodePackages.yaml-language-server
+
+            # Tools
+            git-cliff
+          ];
+
+          RUST_SRC_PATH =
+            "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      });
+}
