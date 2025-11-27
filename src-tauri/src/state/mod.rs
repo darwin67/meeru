7b68a7e4f188b9e03b3ff4a8583d097cc09c4cbd
Darwin D Wu <darwin67@users.noreply.github.com>
2025-11-27 00:15:37 -0800

    create constructor for app state

--- src-tauri/src/state/mod.rs
+++ src-tauri/src/state/mod.rs
@@ -1,1 +1,19 @@
+use crate::db::Database;
 
+use anyhow::Result;
+
+use std::path::PathBuf;
+
+pub struct State {
+    pub db: Database,
+}
+
+impl State {
+    pub async fn new() -> Result<Self> {
+        // TODO provide config and reference that path instead
+        let path = PathBuf::from("meeru.db");
+        let db = Database::new(path).await?;
+
+        Ok(Self { db })
+    }
+}
